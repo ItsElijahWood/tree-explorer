9519dec6c5d3419bb1698d5548126a050d3bbb39
ItsElijahWood <itselijahwood@gmail.com>
2025-03-10 12:22:38 +0000

    feat: Formatted code.

--- src/fetch_files.ts
+++ src/fetch_files.ts
@@ -1,19 +1,24 @@
 import fs from "fs";
 import path from "path";
 
-// Fetch the files, then into 
-// the tree format 
+// Fetch the files, then into
+// the tree format
 export function fetchFiles(directory: string, depth: number = 0): string[] {
   const files = fs.readdirSync(directory);
   let tree: string[] = [];
 
-  files.forEach(file => {
+  files.forEach((file) => {
     const dirFile = path.join(directory, file);
     const stats = fs.statSync(dirFile);
-    const indentation = ' '.repeat(depth * 2);
-   
+    const indentation = " ".repeat(depth * 2);
+
     if (stats.isDirectory()) {
-      if (file.includes("node_modules") || file.includes("vendor") || file.includes(".git")) return;
+      if (
+        file.includes("node_modules") ||
+        file.includes("vendor") ||
+        file.includes(".git")
+      )
+        return;
       tree.push(`${indentation} ${file}/\n`);
       tree = tree.concat(fetchFiles(dirFile, depth + 1));
     } else {
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { ascii } from "./ascii";
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 console.log(ascii());
