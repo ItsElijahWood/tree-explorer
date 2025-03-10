d2f6a9c34b1bb72b6fe4ced65c7645e7154f42d3
ItsElijahWood <itselijahwood@gmail.com>
2025-03-10 12:07:42 +0000

    feat: Improved and customized terminal.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-import { fetchFiles } from "./fetch_files";
 import readline from "readline";
+
+import { fetchFiles } from "./fetch_files";
+import { ascii } from "./ascii";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+console.log(ascii());
+
 // Sends input to fetchFiles function
-rl.question("Full path to the directory: ", (path: string) => {
+rl.question("Complete directory path: ", (path: string) => {
+  path = path.trim();
+
+  if (path.length <= 0) {
+    console.error("Error parameter passed is empty.");
+    rl.close();
+
+    return;
+  }
+
   console.log(`${fetchFiles(path)}`);
 
   rl.close();
